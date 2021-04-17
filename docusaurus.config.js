1c0c56a539fe86be6b497a6252b19d5091ce6ed7
CodeDoctor <wusel905@gmail.com>
2021-04-17 05:39:27 +0000

    update dependencies

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,6 +8,36 @@ module.exports = {
   favicon: 'img/isar.svg',
   organizationName: 'isar',
   projectName: 'isar',
+  plugins: [
+    [
+      '@docusaurus/plugin-pwa',
+      {
+        debug: true,
+        offlineModeActivationStrategies: [
+          'appInstalled',
+          'standalone',
+          'queryString',
+        ],
+        pwaHead: [
+          {
+            tagName: 'link',
+            rel: 'icon',
+            href: '/img/docusaurus.png',
+          },
+          {
+            tagName: 'link',
+            rel: 'manifest',
+            href: '/manifest.json', // your PWA manifest
+          },
+          {
+            tagName: 'meta',
+            name: 'theme-color',
+            content: 'rgb(37, 194, 160)',
+          },
+        ],
+      },
+    ],
+  ],
   themeConfig: {
     navbar: {
       title: 'Isar Database',
