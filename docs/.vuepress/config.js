e706a3febc702429f5b6cb8445aa1d5dc3ba8e33
Simon Leier <simonleier@gmail.com>
2022-01-13 13:52:50 +0100

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -24,7 +24,17 @@ module.exports = {
             'meta',
             { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
         ],
-        ['script', { type: 'text/javascript', src: 'https://static.hotjar.com/c/hotjar-2781644.js?sv=6', async: 'true' }],
+        [
+            'script', {},
+            `(function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:2781644,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
+        ]
     ],
     locales: {
         '/': {
